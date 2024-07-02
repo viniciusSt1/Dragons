1470931589857c7f3bcf5c4b9a2a96341e75fbf3
Alysson da Silva Machado <165211928+machadomil@users.noreply.github.com>
2024-07-02 15:46:58 -0300

    Food.py

--- gameDragons/Food.py
+++ gameDragons/Food.py
@@ -1,22 +1,15 @@
 import random
 
 class Food:
-    def __init__(self, coords):
+    def __init__(self,coords):
         self.position = coords
 
-    def spawn_food(self, display_size, snake_positions):
-        while True:
-            x = random.randrange(1, (display_size[0] // 32)) * 32
-            y = random.randrange(1, (display_size[1] // 32)) * 32
-            self.position = [x, y]
-
-            # Check if food position overlaps with snake positions
-            if self.position not in snake_positions:
-                break
-
-    def draw_food(self, display, surf):
+    def spawm_food(self,display_size):
+        self.position = [random.randrange(1, (display_size[0]//32)) * 32, random.randrange(1, (display_size[1]//32)) * 32]
+    
+    def draw_food(self,display,surf):
         display.window.blit(surf.dragonFruit, self.position)
-
+    
     def to_dict(self):
         return {
             'position': self.position
